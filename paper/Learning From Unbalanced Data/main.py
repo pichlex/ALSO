34718636cc5636e643a2f1413b6c76d76042c332
Alexander Pichugin <pichugin.ad@phystech.edu>
2025-12-16 18:30:59 +0300

    SM temperature

--- paper/Learning From Unbalanced Data/main.py
+++ paper/Learning From Unbalanced Data/main.py
@@ -66,11 +66,12 @@ def run_optimization(
         pi_threshold = config.get("pi_threshold", "none")
         pi_sampling = config.get("pi_sampling", "none")
         pi_strategy = config.get("pi_strategy", "desc")
+        pi_temperature = config.get("pi_temperature", None)
         # mlflow.set_experiment(f"{experiment_base}_seed{config['seed']}_batch_size{config['batch_size']}_dyn_batch={dynamic_batch}_pi_threshold={pi_threshold}_pi_sampling={pi_sampling}")
-        mlflow.set_experiment(f"{experiment_base}_tail_cutoff_seed{config['seed']}_dyn_batch={dynamic_batch}_pi_threshold={pi_threshold}_pi_sampling={pi_sampling}_pi_strategy={pi_strategy}")
+        mlflow.set_experiment(f"{experiment_base}_tail_cutoff_seed{config['seed']}_pi_temp={pi_temperature}_dyn_batch={dynamic_batch}_pi_threshold={pi_threshold}_pi_sampling={pi_sampling}_pi_strategy={pi_strategy}")
         run_title = (
             f"{config.get('run_name', config['optimizer'])}"
-            f"_uc{config.get('unbalance_coef', 'na')}_tail_cutoff_seed{config['seed']}_batch_size{config['batch_size']}_dyn_batch={dynamic_batch}_pi_threshold={pi_threshold}_pi_sampling={pi_sampling}_pi_strategy={pi_strategy}"
+            f"_uc{config.get('unbalance_coef', 'na')}_tail_cutoff_seed{config['seed']}_pi_temp={pi_temperature}_batch_size{config['batch_size']}_dyn_batch={dynamic_batch}_pi_threshold={pi_threshold}_pi_sampling={pi_sampling}_pi_strategy={pi_strategy}"
         )
         mlflow_ctx = mlflow.start_run(run_name=run_title)
 
